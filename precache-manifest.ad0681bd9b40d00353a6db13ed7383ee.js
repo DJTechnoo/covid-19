c3f94de805d00d064d352dfbd5ad6d0e0593734c
askelej <askeltechno@hotmail.com>
2020-05-28 03:30:26 +0200

    Updates

---
+++ precache-manifest.ad0681bd9b40d00353a6db13ed7383ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831ad52faffbcdd14a01bc28eb5100dc",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "ae3433f7fd8bdbd4f7d9",
+    "url": "/covid-19/static/css/main.72ef4db9.chunk.css"
+  },
+  {
+    "revision": "dbaf72707307ae5e3e6d",
+    "url": "/covid-19/static/js/2.7da9f2c1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.7da9f2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3433f7fd8bdbd4f7d9",
+    "url": "/covid-19/static/js/main.1a367a19.chunk.js"
+  },
+  {
+    "revision": "83ba3fa5f0ca432dc476",
+    "url": "/covid-19/static/js/runtime-main.a157d5bc.js"
+  }
+]);
